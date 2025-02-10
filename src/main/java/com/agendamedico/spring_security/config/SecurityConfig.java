57834f88674b5aa2d7d7d993f76e78e0d3eab8a0
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-02-09 21:52:47 -0300

    Lidando com o login e o logout

--- src/main/java/com/agendamedico/spring_security/config/SecurityConfig.java
+++ src/main/java/com/agendamedico/spring_security/config/SecurityConfig.java
@@ -14,14 +14,18 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-    http.authorizeRequests(authorizeRequests -> authorizeRequests
+    // acessos públicos liberados
+    http.authorizeHttpRequests(authorize -> authorize
         .requestMatchers("/webjars/**", "/css/**", "/image/**", "/js/**").permitAll()
         .requestMatchers("/", "/home").permitAll()
         .anyRequest().authenticated())
         .formLogin(formLogin -> formLogin
             .loginPage("/login")
-            .permitAll());
+            .defaultSuccessUrl("/", true)
+            .failureUrl("/login-error")
+            .permitAll())
+        .logout(logout -> logout
+            .logoutSuccessUrl("/"));
     return http.build();
 
   }
