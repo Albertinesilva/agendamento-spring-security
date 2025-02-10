b6287cf58b0adfef9144dce3af96626580e5d553
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-02-09 22:25:30 -0300

    Verificando a autenticidade do login

--- src/main/java/com/agendamedico/spring_security/service/UsuarioService.java
+++ src/main/java/com/agendamedico/spring_security/service/UsuarioService.java
@@ -1,19 +1,71 @@
 package com.agendamedico.spring_security.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.agendamedico.spring_security.domain.Perfil;
 import com.agendamedico.spring_security.domain.Usuario;
 import com.agendamedico.spring_security.repository.UsuarioRepository;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
 
   @Autowired
   private UsuarioRepository usuarioRepository;
 
+  @Transactional(readOnly = true)
   public Usuario buscarPorEmail(String email) {
     return usuarioRepository.findByEmail(email);
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Usuario usuario = usuarioRepository.findByEmail(username);
+    // return new User(
+    //     usuario.getEmail(),
+    //     usuario.getSenha(),
+    //     AuthorityUtils.createAuthorityList(getAuthorities(usuario.getPerfis()))
+    // );
+
+    // Versão mais enxuta com stream().map():
+    return new User(
+        usuario.getEmail(),
+        usuario.getSenha(),
+        usuario.getPerfis().stream()
+            .map(Perfil::getDesc)
+            .map(SimpleGrantedAuthority::new)
+            .toList()
+    );
+  }
+
+  @SuppressWarnings("unused")
+  private String[] getAuthorities(List<Perfil> perfis) {
+    
+    // Versão mais enxuta com stream().map():
+    return perfis.stream().map(Perfil::getDesc).toArray(String[]::new);
+
+    // Versão com for tradicional
+    // String[] authorities = new String[perfis.size()];
+    // for (int i = 0; i < perfis.size(); i++) {
+    //   authorities[i] = perfis.get(i).getDesc();
+    // }
+    // return authorities;
+
+    // Versão com forEach
+    // List<String> authoritiesList = new ArrayList<>();
+    // perfis.forEach(perfil -> authoritiesList.add(perfil.getDesc()));
+    // return authoritiesList.toArray(new String[0]);
+
   }
 
 }
