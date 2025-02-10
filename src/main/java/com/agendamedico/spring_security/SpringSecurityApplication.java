1abc0c2344a455c3fbefe8bd471e1031910c35ea
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-02-09 22:55:52 -0300

    Lidando com a criptografica se senha

--- src/main/java/com/agendamedico/spring_security/SpringSecurityApplication.java
+++ src/main/java/com/agendamedico/spring_security/SpringSecurityApplication.java
@@ -2,12 +2,14 @@ package com.agendamedico.spring_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
+		// SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
 }
--- src/main/java/com/agendamedico/spring_security/config/SecurityConfig.java
+++ src/main/java/com/agendamedico/spring_security/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package com.agendamedico.spring_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.agendamedico.spring_security.service.UsuarioService;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfig {
+
+  @Autowired
+  private UsuarioService usuarioService;
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -28,6 +41,23 @@ public class SecurityConfig {
             .logoutSuccessUrl("/"));
     return http.build();
 
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      UserDetailsService userDetailsService,
+      PasswordEncoder passwordEncoder) {
+
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder);
+
+    return new ProviderManager(authProvider);
   }
 
 }
