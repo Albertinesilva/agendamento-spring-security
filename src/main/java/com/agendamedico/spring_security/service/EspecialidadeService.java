78ef5fb8d84d48954c5aca2c89c3c3d65184c04d
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-03-08 10:24:56 -0300

    Listando Especialidades

--- src/main/java/com/agendamedico/spring_security/service/EspecialidadeService.java
+++ src/main/java/com/agendamedico/spring_security/service/EspecialidadeService.java
@@ -1,11 +1,18 @@
 package com.agendamedico.spring_security.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.agendamedico.spring_security.datatables.Datatables;
+import com.agendamedico.spring_security.datatables.DatatablesColunas;
 import com.agendamedico.spring_security.domain.Especialidade;
 import com.agendamedico.spring_security.repository.EspecialidadeRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class EspecialidadeService {
@@ -13,9 +20,22 @@ public class EspecialidadeService {
   @Autowired
   private EspecialidadeRepository especialidadeRepository;
 
+  @Autowired
+  private Datatables dataTables;
+
   @Transactional(readOnly = false)
   public void salvar(Especialidade especialidade) {
     especialidadeRepository.save(especialidade);
+  }
+
+  @Transactional(readOnly = true)
+  public Map<String, Object> buscarEspecialidades(HttpServletRequest request) {
+    dataTables.setRequest(request);
+    dataTables.setColunas(DatatablesColunas.ESPECIALIDADES);
+    Page<?> page = dataTables.getSearch().isEmpty()
+        ? especialidadeRepository.findAll(dataTables.getPageable())
+        : especialidadeRepository.findAllByTitulo(dataTables.getSearch(), dataTables.getPageable());
+    return dataTables.getResponse(page);
   }
 
 }
--- src/main/java/com/agendamedico/spring_security/web/controller/EspecialidadeController.java
+++ src/main/java/com/agendamedico/spring_security/web/controller/EspecialidadeController.java
@@ -1,6 +1,7 @@
 package com.agendamedico.spring_security.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,14 +11,16 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.agendamedico.spring_security.domain.Especialidade;
 import com.agendamedico.spring_security.service.EspecialidadeService;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("especialidades")
 public class EspecialidadeController {
-  
+
   @Autowired
   private EspecialidadeService especialidadeService;
 
-  @GetMapping({"", "/"})
+  @GetMapping({ "", "/" })
   public String abrir(Especialidade especialidade) {
     return "especialidade/especialidade";
   }
@@ -27,5 +30,10 @@ public class EspecialidadeController {
     especialidadeService.salvar(especialidade);
     attr.addFlashAttribute("sucesso", "Operação realizada com sucesso.");
     return "redirect:/especialidades";
+  }
+
+  @GetMapping("/datatables/server")
+  public ResponseEntity<?> getEspecialidades(HttpServletRequest request) {
+    return ResponseEntity.ok(especialidadeService.buscarEspecialidades(request));
   }
 }
