cdde79e038ecd78c08a40c9e0b7f79da46b1f604
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-03-08 16:20:04 -0300

    Conversor para a Lista de Perfis e Cadastro de novos usuários

--- src/main/java/com/agendamedico/spring_security/web/controller/UsuarioController.java
+++ src/main/java/com/agendamedico/spring_security/web/controller/UsuarioController.java
@@ -1,11 +1,18 @@
 package com.agendamedico.spring_security.web.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.agendamedico.spring_security.domain.Perfil;
 import com.agendamedico.spring_security.domain.Usuario;
 import com.agendamedico.spring_security.service.UsuarioService;
 
@@ -34,5 +41,24 @@ public class UsuarioController {
   @GetMapping("/datatables/server/usuarios")
   public ResponseEntity<?> listaUsuariosDatatables(HttpServletRequest request) {
     return ResponseEntity.ok(usuarioService.buscarUsuarios(request));
+  }
+
+  // salvar cadastro de usuários por administrador
+  @PostMapping("/cadastro/salvar")
+  public String salvarUsuarios(Usuario usuario, RedirectAttributes attr) {
+
+    if (usuario.getPerfis().size() > 2 ||
+        usuario.getPerfis().stream().map(Perfil::getId).collect(Collectors.toSet())
+            .containsAll(Arrays.asList(1L, 3L))
+        ||
+        usuario.getPerfis().stream().map(Perfil::getId).collect(Collectors.toSet())
+            .containsAll(Arrays.asList(2L, 3L))) {
+      attr.addFlashAttribute("falha", "Paciente não pode ser Admin e/ou Médico!");
+      attr.addFlashAttribute("usuario", usuario);
+    } else {
+      usuarioService.salvarUsuario(usuario);
+      attr.addFlashAttribute("sucesso", "Operação realizada com sucesso!");
+    }
+    return "redirect:/u/novo/cadastro/usuario";
   }
 }
