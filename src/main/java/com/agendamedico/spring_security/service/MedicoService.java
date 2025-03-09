0914c3f54ab77c5597c6c8102cdb3e4857e06950
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-03-08 22:14:17 -0300

    Métodos Salvar e Editar para Médico

--- src/main/java/com/agendamedico/spring_security/service/MedicoService.java
+++ src/main/java/com/agendamedico/spring_security/service/MedicoService.java
@@ -17,4 +17,34 @@ public class MedicoService {
   public Medico buscarPorUsuarioId(Long id) {
     return medicoRepository.findByUsuarioId(id).orElse(new Medico());
   }
+
+  @Transactional(readOnly = false)
+  public void salvar(Medico medico) {
+    medicoRepository.save(medico);
+  }
+
+  /**
+   * Edita um médico existente no banco de dados.
+   * 
+   * <p>
+   * Não é necessário chamar {@code medicoRepository.save(medico)} porque o
+   * Hibernate
+   * mantém a entidade no estado persistente. Qualquer alteração feita no objeto
+   * será
+   * automaticamente sincronizada com o banco de dados no final da transação.
+   * </p>
+   *
+   * @param medico Objeto {@link Medico} contendo os dados atualizados.
+   */
+  @Transactional(readOnly = false)
+  public void editar(Medico medico) {
+    Medico m2 = medicoRepository.findById(medico.getId()).get();
+    m2.setCrm(medico.getCrm());
+    m2.setDtInscricao(medico.getDtInscricao());
+    m2.setNome(medico.getNome());
+    if (!medico.getEspecialidades().isEmpty()) {
+      m2.getEspecialidades().addAll(medico.getEspecialidades());
+    }
+  }
+
 }
--- src/main/java/com/agendamedico/spring_security/web/controller/MedicoController.java
+++ src/main/java/com/agendamedico/spring_security/web/controller/MedicoController.java
@@ -1,20 +1,45 @@
 package com.agendamedico.spring_security.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.agendamedico.spring_security.domain.Medico;
+import com.agendamedico.spring_security.service.MedicoService;
 
 @Controller
 @RequestMapping("/medicos")
 public class MedicoController {
 
-  // abrir pagina de dados pessoais de medicos pelo MEDICO
-  @RequestMapping("/dados")
-  public String abrirPorMedico(Medico medico, ModelMap model) {
+  @Autowired
+  private MedicoService medicoService;
 
+  // abrir pagina de dados pessoais de medicos pelo MEDICO
+  @GetMapping("/dados")
+  public String abrirPorMedico(Medico medico, ModelMap model) {
     return "medico/cadastro";
+  }
+
+  // salvar dados pessoais de medicos pelo MEDICO
+  @PostMapping("/salvar")
+  public String salvar(Medico medico, RedirectAttributes attr) {
+    medicoService.salvar(medico);
+    attr.addFlashAttribute("success", "Medico inserido com sucesso.");
+    attr.addFlashAttribute("medico", medico);
+    return "redirect:/medicos/dados";
+  }
+
+  // editar dados pessoais de medicos pelo MEDICO
+  @PostMapping("/editar")
+  public String editar(Medico medico, RedirectAttributes attr) {
+    medicoService.editar(medico);
+    attr.addFlashAttribute("success", "Operação realizada com sucesso.");
+    attr.addFlashAttribute("medico", medico);
+    return "redirect:/medicos/dados";
   }
 
 }
