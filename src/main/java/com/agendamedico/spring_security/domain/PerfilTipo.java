7ebb9a1534b44a57cf2e3dc2d2bedce8a9c1ed75
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-03-17 12:20:07 -0300

    Enum PerfilTipo JavaDoc

--- src/main/java/com/agendamedico/spring_security/domain/PerfilTipo.java
+++ src/main/java/com/agendamedico/spring_security/domain/PerfilTipo.java
@@ -1,20 +1,51 @@
 package com.agendamedico.spring_security.domain;
 
+/**
+ * Enum que representa os tipos de perfil de usuário no sistema.
+ * Cada tipo de perfil tem um código único e uma descrição.
+ */
 public enum PerfilTipo {
-	ADMIN(1, "ADMIN"), MEDICO(2, "MEDICO"), PACIENTE(3, "PACIENTE");
 
+	/** Representa o perfil de administrador. */
+	ADMIN(1, "ADMIN"),
+
+	/** Representa o perfil de médico. */
+	MEDICO(2, "MEDICO"),
+
+	/** Representa o perfil de paciente. */
+	PACIENTE(3, "PACIENTE");
+
+	/** Código único associado ao perfil. */
 	private long cod;
+
+	/** Descrição do perfil. */
 	private String desc;
 
+	/**
+	 * Construtor privado para inicializar os valores do tipo de perfil.
+	 * 
+	 * @param cod  código único associado ao perfil.
+	 * @param desc descrição do perfil.
+	 */
 	private PerfilTipo(long cod, String desc) {
 		this.cod = cod;
 		this.desc = desc;
 	}
 
+	/**
+	 * Obtém o código do perfil.
+	 * 
+	 * @return o código associado ao perfil.
+	 */
 	public long getCod() {
 		return cod;
 	}
 
+	/**
+	 * Obtém a descrição do perfil.
+	 * 
+	 * @return a descrição do perfil.
+	 */
 	public String getDesc() {
 		return desc;
 	}
