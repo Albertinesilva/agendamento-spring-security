164c0e601a0cceb849d172ca854d5a9ed14fd9c7
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-03-17 12:27:14 -0300

    Repositories Com JavaDoc

--- src/main/java/com/agendamedico/spring_security/repository/MedicoRepository.java
+++ src/main/java/com/agendamedico/spring_security/repository/MedicoRepository.java
@@ -11,9 +11,24 @@ import com.agendamedico.spring_security.domain.Medico;
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
+  /**
+   * Busca um médico pelo ID do usuário associado a ele.
+   *
+   * @param id ID do usuário associado ao médico.
+   * @return Um objeto Optional contendo o médico encontrado, ou vazio se não
+   *         encontrado.
+   */
   @Query("select m from Medico m where m.usuario.id = :id")
   Optional<Medico> findByUsuarioId(Long id);
 
+  /**
+   * Busca um médico pelo e-mail do usuário associado a ele.
+   *
+   * @param email E-mail do usuário associado ao médico.
+   * @return Um objeto Optional contendo o médico encontrado, ou vazio se não
+   *         encontrado.
+   */
   @Query("select m from Medico m where m.usuario.email like :email")
   Optional<Medico> findByUsuarioEmail(String email);
+
 }
