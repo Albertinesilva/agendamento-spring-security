89bad779678fdb83966df16fb90beffe0659276e
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-03-20 19:08:34 -0300

    Métodos de Testes de Envio de Email Retirados da Class SpringSecurityApplication

--- src/main/java/com/agendamedico/spring_security/SpringSecurityApplication.java
+++ src/main/java/com/agendamedico/spring_security/SpringSecurityApplication.java
@@ -1,28 +1,13 @@
 package com.agendamedico.spring_security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-
-import com.agendamedico.spring_security.service.EmailService;
 
 @SpringBootApplication
-public class SpringSecurityApplication implements CommandLineRunner {
+public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
 		// System.out.println(new BCryptPasswordEncoder().encode("123456"));
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
-
-	@Autowired
-	EmailService emailService;
-
-	@Override
-	public void run(String... args) throws Exception {
-		emailService.enviarPedidoDeConfirmacaoDeCadastro("no.reply.smttsaj@gmail.com", "998877");
-	}
-
 }
