939e49652ccbedfc2373af83914650f89d6be915
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-03-20 16:44:44 -0300

    Configuração o Serviço de e-mail via SMTTP

--- src/main/java/com/agendamedico/spring_security/SpringSecurityApplication.java
+++ src/main/java/com/agendamedico/spring_security/SpringSecurityApplication.java
@@ -1,14 +1,30 @@
 package com.agendamedico.spring_security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
-public class SpringSecurityApplication {
+public class SpringSecurityApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		// System.out.println(new BCryptPasswordEncoder().encode("123456"));
 		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+	@Autowired
+	JavaMailSender sender;
+
+	@Override
+	public void run(String... args) throws Exception {
+		SimpleMailMessage simple = new SimpleMailMessage();
+		simple.setTo("no.reply.smttsaj@gmail.com");
+		simple.setText("Teste número 1");
+		simple.setSubject("teste 1");
+		sender.send(simple);
 	}
 
 }
