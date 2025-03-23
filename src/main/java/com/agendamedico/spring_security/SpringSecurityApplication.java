66234a8efac6c0f9e050a92d2cb32cab2a3ccff6
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-03-23 00:30:52 -0300

    Site Atualizado

--- src/main/java/com/agendamedico/spring_security/SpringSecurityApplication.java
+++ src/main/java/com/agendamedico/spring_security/SpringSecurityApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*https://clinica-spring-security-b074f1332545.herokuapp.com/login */
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		// System.out.println(new BCryptPasswordEncoder().encode("123456"));
+		// System.out.println(new BCryptPasswordEncoder().encode(""));
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 }
